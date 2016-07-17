c9582425a0d3d1a85722fce74fb52c6f55c57a77
JAndresLM <josandlopmol@hotmail.com>
2016-07-17 15:59:15 -0600

    Archivos de ejemplo para consultas

---
+++ cronjobs/summary.js
@@ -0,0 +1,14 @@
+{
+	"system_id":622396,
+	"modules":8,
+	"size_w":1720,
+	"current_power":233, //watts
+	"energy_today":5193,//watts-hour/1000=kwatts-hour
+	"energy_lifetime":2931749,
+	"summary_date":"2016-07-17",
+	"source":"microinverters",
+	"status":"normal",
+	"operational_at":1430245500,
+	"last_report_at":1468791894,
+	"last_interval_end_at":1468791853
+}
