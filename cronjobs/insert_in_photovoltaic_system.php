5a157f1a9ae9788be618672262acbd3d108e2f1f
JAndresLM <josandlopmol@hotmail.com>
2016-08-28 13:44:55 -0600

    Script del sistema fotovoltaico finalizado, script del sistema termico modificado

--- cronjobs/insert_in_photovoltaic_system.php
+++ cronjobs/insert_in_photovoltaic_system.php
@@ -1,3 +1,37 @@
 <?php
-	echo "Hello Gaudy";
+	$today = time()-86400;
+	$end = $today+50400;
+	$data = json_decode(file_get_contents("https://api.enphaseenergy.com/api/v2/systems/622396/stats?start_at=$today&end_at=$end&datetime_format=iso8601&key=351796adc7e7e2106347c77c7da120be&user_id=4e4463794f444d300a"), true);
+
+	$place = 2;
+	$strconn="host=localhost port=5432 dbname=dbasistelectro user=postgres password=123456";
+	$conn=pg_connect($strconn) or die ("<strong>Ha ocurrido un error en el acceso a la base de datos.</strong>");
+	$date = "";
+	$kw = 0.0;
+
+	$tam = sizeof($data["intervals"]);
+	$sum=0;	
+	for ($i = 0; $i < $tam; $i++) {
+		$min = substr($data["intervals"][$i]["end_at"],14,-9);
+		$value = $data["intervals"][$i]["enwh"];
+		if ($min==00 or $min==15 or $min==30 or $min==45){
+			$sum = $sum + $value;
+			$kw = $sum/1000;
+			//Insert
+			$date = substr($data["intervals"][$i]["end_at"],0,-15)." ".substr($data["intervals"][$i]["end_at"],11,-6);
+			$query = "insert into sys_photovoltaic(place_id,date_time,kw_produced) values ($place,'$date',$kw)";
+			$result = pg_query($conn,$query) or die ("<strong>Error durante la consulta.</strong>".pg_last_error());
+			//Reset sum			
+			$sum = 0; 
+		}else{
+			$sum = $sum + $value;
+			if ($sum!=0 and $i==$tam-1){
+				//last insert
+				$kw = $sum/1000;
+				$date = substr($data["intervals"][$i]["end_at"],0,-15)." ".substr($data["intervals"][$i]["end_at"],11,-6);
+				$query = "insert into sys_photovoltaic(place_id,date_time,kw_produced) values ($place,'$date',$kw)";
+				$result = pg_query($conn,$query) or die ("<strong>Error durante la consulta.</strong>".pg_last_error());
+			} 
+		}
+	}
 ?>
--- cronjobs/insert_in_thermal_system.php
+++ cronjobs/insert_in_thermal_system.php
@@ -1,8 +1,8 @@
 <?php
-	/*$data = json_decode(file_get_contents("http://172.24.18.15/dlx/download/live?sessionAuthUsername=admin&sessionAuthPassword=admin&outputType=json&source=current"), true);*/
-	$data = json_decode(file_get_contents("http://localhost/AsistElectro/cronjobs/live.txt"), true);
+	$data = json_decode(file_get_contents("http://172.24.18.15/dlx/download/live?sessionAuthUsername=admin&sessionAuthPassword=admin&outputType=json&source=current"), true);
+	//$data = json_decode(file_get_contents("http://localhost/AsistElectro/cronjobs/live.txt"), true);
 
-	$date = date("Y-m-d H:i:s",($data["headersets"][0]["timestamp"]));
+	$date = date("Y-m-d H:i:s",($data["headersets"][0]["timestamp"])-3600);
 	$s1 = $data["headersets"][0]["packets"][0]["field_values"][0]["value"];
 	$s2 = $data["headersets"][0]["packets"][0]["field_values"][1]["value"];
 	$s3 = $data["headersets"][0]["packets"][0]["field_values"][2]["value"];
