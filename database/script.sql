552175e1ca8cd4c4365844be0c2c608610a4a9b9
JAndresLM <josandlopmol@hotmail.com>
2016-10-25 00:14:23 -0600

    Nuevos cambios en la BD, restriccion unica de varias columnas

--- database/script.sql
+++ database/script.sql
@@ -4,7 +4,7 @@ CREATE DATABASE dbAsistElectro;
 --DROP TABLE users;
 CREATE TABLE users(
    usern		VARCHAR(25),
-   passw		VARCHAR(25),
+   passw		VARCHAR(25) NOT NULL UNIQUE,
    fullname	VARCHAR(50)	NOT NULL,
    PRIMARY KEY(usern)
 );
@@ -19,19 +19,20 @@ CREATE TABLE places(
 --DROP TABLE sys_photovoltaic;
 CREATE TABLE sys_photovoltaic(
    id_sys_foto	SERIAL,
-   place_id	INT REFERENCES places(id_place) ON UPDATE NO ACTION ON DELETE NO ACTION,
-   date_time	TIMESTAMP  UNIQUE,
+   place_id	INT NOT NULL REFERENCES places(id_place) ON UPDATE NO ACTION ON DELETE NO ACTION,
+   date_time	TIMESTAMP NOT NULL,
    kw_produced DECIMAL,
    kw_consumed DECIMAL,
    radiation	DECIMAL,
-   PRIMARY KEY(id_sys_foto)
+   PRIMARY KEY(id_sys_foto),
+   CONSTRAINT photo_constraint UNIQUE (place_id, date_time)
 );
 
 --DROP TABLE sys_photovoltaic_config;
 CREATE TABLE sys_photovoltaic_config(
    id_sys_foto_config SERIAL,
    sys_description VARCHAR(100) NOT NULL,
-   place_id INT UNIQUE REFERENCES places(id_place) ON UPDATE NO ACTION ON DELETE NO ACTION,
+   place_id INT NOT NULL UNIQUE REFERENCES places(id_place) ON UPDATE NO ACTION ON DELETE NO ACTION,
    sys_number  INT NOT NULL UNIQUE,
    sys_user VARCHAR(100) NOT NULL UNIQUE,
    sys_key VARCHAR(100) NOT NULL UNIQUE,
@@ -41,19 +42,20 @@ CREATE TABLE sys_photovoltaic_config(
 --DROP TABLE sys_thermal;
 CREATE TABLE sys_thermal(
    id_sys_term	SERIAL,
-   place_id	INT REFERENCES places(id_place) ON UPDATE NO ACTION ON DELETE NO ACTION,
-   date_time	TIMESTAMP UNIQUE,
+   place_id	INT NOT NULL REFERENCES places(id_place) ON UPDATE NO ACTION ON DELETE NO ACTION,
+   date_time	TIMESTAMP NOT NULL,
    sensor1 DECIMAL,
    sensor2 DECIMAL,
    sensor3 DECIMAL,
-   PRIMARY KEY(id_sys_term)
+   PRIMARY KEY(id_sys_term),
+   CONSTRAINT therm_constraint UNIQUE (place_id, date_time)
 );
 
 --DROP TABLE sys_thermal_config;
 CREATE TABLE sys_thermal_config(
    id_sys_term_config SERIAL,
    sys_description VARCHAR(100) NOT NULL,
-   place_id INT UNIQUE REFERENCES places(id_place) ON UPDATE NO ACTION ON DELETE NO ACTION,
+   place_id INT NOT NULL UNIQUE REFERENCES places(id_place) ON UPDATE NO ACTION ON DELETE NO ACTION,
    sys_url  VARCHAR(100) NOT NULL UNIQUE,
    sys_user VARCHAR(100) NOT NULL,
    sys_pass VARCHAR(100) NOT NULL,
